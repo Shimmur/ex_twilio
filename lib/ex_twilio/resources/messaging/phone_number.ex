aa407bcca4371852ad8c3d7ecab794386825e5f5
kocitomas <tomas@shimmur.com>
2018-11-01 14:53:56 -0400

    initial work on service phone number subresource

---
+++ lib/ex_twilio/resources/messaging/phone_number.ex
@@ -0,0 +1,43 @@
+defmodule ExTwilio.Messaging.PhoneNumber do
+  @moduledoc """
+  Represents an PhoneNumber sub-resource associated with a messaging service.
+
+  - [Twilio docs](https://www.twilio.com/docs/proxy/api/phone-number?code-sample=code-fetch-phone-number&code-language=curl&code-sdk-version=json)
+  """
+
+  defstruct sid: nil,
+            account_sid: nil,
+            service_sid: nil,
+            date_created: nil,
+            date_updated: nil,
+            friendly_name: nil,
+            phone_number: nil,
+            api_version: nil,
+            voice_caller_id_lookup: nil,
+            voice_url: nil,
+            voice_method: nil,
+            voice_fallback_url: nil,
+            status_callback: nil,
+            status_callback_method: nil,
+            voice_application_sid: nil,
+            sms_url: nil,
+            sms_method: nil,
+            sms_fallback_url: nil,
+            sms_fallback_method: nil,
+            sms_application_sid: nil,
+            capabilities: nil,
+            address_requirements: nil,
+            uri: nil
+
+  use ExTwilio.Resource,
+      import: [
+        :stream,
+        :all,
+        :find,
+        :create,
+        :update,
+        :destroy
+      ]
+
+  def parents, do: [:service]
+end
